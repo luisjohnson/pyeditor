5564c49c3183457069d8e8fdac800bccf2b5b3da
Luis Johnson <luisjohnson@me.com>
2016-04-15 19:16:32 -0700

    New, open and save documet funcionality added

--- minimum.py
+++ minimum.py
@@ -1,3 +1,4 @@
+#
 import sys
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import Qt
@@ -7,6 +8,8 @@ class Main(QtGui.QMainWindow):
     def __init__(self, parent = None):
         QtGui.QMainWindow.__init__(self, parent)
 
+        self.filename = ""
+        
         self.initUI()
 
     def  initToolbar(self):
@@ -23,8 +26,25 @@ class Main(QtGui.QMainWindow):
 
         menubar = self.menuBar()
 
+        self.newAction = QtGui.QAction("New", self)
+        self.newAction.setShortcut("Ctrl+N")
+        self.newAction.triggered.connect(self.new)
+         
+        self.openAction = QtGui.QAction("Open", self)
+        self.openAction.setShortcut("Ctrl+O")
+        self.openAction.triggered.connect(self.open)
+              
+        self.saveAction = QtGui.QAction("Save", self)
+        self.saveAction.setShortcut("Ctrl+S")
+        self.saveAction.triggered.connect(self.save)
+
+
         file = menubar.addMenu("File")
-        file.addAction()
+
+        file.addAction(self.newAction)
+        file.addAction(self.openAction)
+        file.addAction(self.saveAction)
+        
 
         
         edit = menubar.addMenu("Edit")
@@ -48,6 +68,34 @@ class Main(QtGui.QMainWindow):
         self.setGeometry(100,100,1030,800)
 
         self.setWindowTitle("pyEditor")
+
+        
+    def new(self):
+
+        spawn = Main(self)
+        spawn.show()
+
+    def open(self):
+
+        self.filename = QtGui.QFileDialog.getOpenFileName(self, 'Open file', ".", "(*.writer)")
+
+        if self.filename:
+            with open(self.filename, "rt") as file:
+                self.text.setText(file.read())
+
+    def save(self):
+
+        if not self.filename:
+            self.filename = QtGui.QFileDialog.getSaveFileName(self, "Save File")
+
+        if not self.filename.endswith("*.write"):
+            self.filename += ".writer"
+
+        with open(self.filename, "wt") as file:
+            file.write(self.text.toHtml())
+                
+            
+        
 
 def main():
 
